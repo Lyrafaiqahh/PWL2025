29d82d5ca3d4015056a508a6efe12c1ff77ad0b7
Lyrafaiqahh <lyrafaiqah@gmail.com>
2025-03-13 06:38:48 +0700

    Praktikum 2.3 – Retreiving Aggregrates

--- Minggu4/JS4/app/Http/Controllers/UserController.php
+++ Minggu4/JS4/app/Http/Controllers/UserController.php
@@ -10,10 +10,12 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data' => $user]);
+        $userCount = UserModel::where('level_id', 2)->count();
+        return view('user', ['jumlah_pengguna' => $userCount]);
     }
 }
+
+
 
 
 
