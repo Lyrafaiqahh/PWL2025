d8e98b01572e36257a798e3f21ebbc4371e0953d
Lyrafaiqahh <lyrafaiqah@gmail.com>
2025-04-17 09:06:52 +0700

    Praktikum-3 Export Pdf data barang

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -125,6 +125,7 @@ Route::middleware(['authorize:ADM,MNG'])->group(function(){
         Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
           // Export Barang with Excel
           Route::get('export_excel', [BarangController::class, 'export_excel']); //export excel
+          Route::get('export_pdf', [BarangController::class, 'export_pdf']); //export pdf
 
 
 });
